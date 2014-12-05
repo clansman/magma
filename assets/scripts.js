638b519d40ba8d16c36e486a075c65ddcdcae334
Zulfat Ilyasov <mail@zulfat.net>
2014-12-06 00:33:03 +0300

    canvas sparks

--- assets/scripts.js
+++ assets/scripts.js
@@ -3,3 +3,102 @@ var parallax, scene;
 scene = document.getElementById('scene');
 
 parallax = new Parallax(scene);
+
+(function() {
+  var Circle, addListeners, animate, animateHeader, canvas, circles, ctx, height, initHeader, largeHeader, resize, scrollCheck, target, width;
+  width = void 0;
+  height = void 0;
+  largeHeader = void 0;
+  canvas = void 0;
+  ctx = void 0;
+  circles = void 0;
+  target = void 0;
+  animateHeader = true;
+  initHeader = function() {
+    var c, x;
+    width = 500;
+    height = 200;
+    target = {
+      x: 0,
+      y: height
+    };
+    canvas = document.getElementById("demo-canvas");
+    canvas.width = width;
+    canvas.height = height;
+    ctx = canvas.getContext("2d");
+    circles = [];
+    x = 0;
+    while (x < width * 0.07) {
+      c = new Circle();
+      circles.push(c);
+      x++;
+    }
+    return animate();
+  };
+  addListeners = function() {
+    return window.addEventListener("scroll", scrollCheck);
+  };
+  scrollCheck = function() {
+    if (document.body.scrollTop > height) {
+      return animateHeader = false;
+    } else {
+      return animateHeader = true;
+    }
+  };
+  resize = function() {
+    width = window.innerWidth;
+    height = window.innerHeight;
+    canvas.width = width;
+    return canvas.height = height;
+  };
+  animate = function() {
+    var i;
+    if (animateHeader) {
+      ctx.clearRect(0, 0, width, height);
+      for (i in circles) {
+        circles[i].draw();
+      }
+    }
+    return requestAnimationFrame(animate);
+  };
+  Circle = (function() {
+    function Circle() {
+      this.pos = {};
+      this.init();
+      console.log(this);
+    }
+
+    Circle.prototype.init = function() {
+      this.pos.x = Math.random() * width;
+      this.pos.y = height + Math.random() * 500;
+      this.alpha = 0.1 + Math.random() * 0.3;
+      this.scale = 0.1 + Math.random() * 0.05;
+      this.velocity = Math.random() * 10;
+      this.life = 20 + Math.random() * 10;
+      this.remaining_life = this.life;
+      return this.speed = {
+        x: -2.5 + Math.random() * 5,
+        y: -3 + Math.random()
+      };
+    };
+
+    Circle.prototype.draw = function() {
+      this.scale -= 0.00065;
+      this.remaining_life -= 0.1;
+      if (this.remaining_life < 0 || this.scale < 0) {
+        this.init();
+      }
+      this.pos.y += this.speed.y;
+      this.pos.x += this.speed.x;
+      ctx.beginPath();
+      ctx.arc(this.pos.x, this.pos.y, this.scale * 10, 0, 2 * Math.PI, false);
+      ctx.fillStyle = "rgba(254,149,0," + 0.8 + ")";
+      return ctx.fill();
+    };
+
+    return Circle;
+
+  })();
+  initHeader();
+  return addListeners();
+})();
--- src/script.coffee
+++ src/script.coffee
@@ -1,2 +1,99 @@
 scene = document.getElementById 'scene'
 parallax = new Parallax scene
+
+(->
+       
+  width = undefined
+  height = undefined
+  largeHeader = undefined
+  canvas = undefined
+  ctx = undefined
+  circles = undefined
+  target = undefined
+  animateHeader = true
+  
+
+  initHeader = ->
+    # width = window.innerWidth
+    # height = window.innerHeight
+    width = 500
+    height = 200
+    target =
+      x: 0
+      y: height
+
+    # largeHeader = document.getElementById("large-header")
+    # largeHeader.style.height = height + "px"
+    canvas = document.getElementById("demo-canvas")
+    canvas.width = width
+    canvas.height = height
+    ctx = canvas.getContext("2d")
+    
+    # create particles
+    circles = []
+    x = 0
+
+    while x < width * 0.07
+      c = new Circle()
+      circles.push c
+      x++
+    animate()
+  
+  # Event handling
+  addListeners = ->
+    window.addEventListener "scroll", scrollCheck
+    # window.addEventListener "resize", resize
+
+  scrollCheck = ->
+    if document.body.scrollTop > height
+      animateHeader = false
+    else
+      animateHeader = true
+
+  resize = ->
+    width = window.innerWidth
+    height = window.innerHeight
+    # largeHeader.style.height = height + "px"
+    canvas.width = width
+    canvas.height = height
+
+  animate = ->
+    if animateHeader
+      ctx.clearRect 0, 0, width, height
+      for i of circles
+        circles[i].draw()
+    requestAnimationFrame animate
+  
+  class Circle
+    constructor: ->
+      @pos = {}
+      @init()
+      console.log @
+
+    init: ->
+      @pos.x = Math.random() * width
+      @pos.y = height + Math.random() * 500
+      @alpha = 0.1 + Math.random() * 0.3
+      @scale = 0.1 + Math.random() * 0.05
+      @velocity = Math.random() * 10
+      @life = 20+Math.random()*10;
+      @remaining_life = @life;
+      @speed = 
+        x: -2.5+Math.random() * 5
+        y: -3+Math.random()
+
+    draw: ->
+      @scale -= 0.00065
+      @remaining_life -= 0.1
+      @init()  if @remaining_life < 0 or @scale < 0
+      @pos.y += @speed.y
+      @pos.x += @speed.x
+      # @alpha -= 0.0015
+      ctx.beginPath()
+      ctx.arc @pos.x, @pos.y, @scale * 10, 0, 2 * Math.PI, false
+      ctx.fillStyle = "rgba(254,149,0," + 0.8 + ")"
+      ctx.fill()
+
+  initHeader()
+  addListeners()
+)()
