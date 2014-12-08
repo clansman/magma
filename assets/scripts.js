0923a452c2a3d5cbe91fd9e9b7ee9c00c34f4554
Zulfat Ilyasov <mail@zulfat.net>
2014-12-08 13:01:22 +0300

    sparks on moseover

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,122 +1,138 @@
-var parallax, scene;
+var Circle, addCircles, addListeners, animate, animateHeader, canvas, circles, ctx, firstRun, getRandom, height, initHeader, largeHeader, onMouseOver, parallax, resize, scene, scrollCheck, target, width, x;
 
 scene = document.getElementById('scene');
 
 parallax = new Parallax(scene);
 
-(function() {
-  var Circle, addListeners, animate, animateHeader, canvas, circles, ctx, firstRun, getRandom, height, initHeader, largeHeader, resize, scrollCheck, target, width;
-  width = void 0;
-  height = void 0;
-  largeHeader = void 0;
-  canvas = void 0;
-  ctx = void 0;
-  circles = void 0;
-  target = void 0;
-  animateHeader = true;
-  getRandom = function(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+width = void 0;
+
+height = void 0;
+
+largeHeader = void 0;
+
+canvas = void 0;
+
+ctx = void 0;
+
+circles = void 0;
+
+target = void 0;
+
+animateHeader = true;
+
+getRandom = function(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+x = 0;
+
+initHeader = function() {
+  width = 300;
+  height = 500;
+  target = {
+    x: 0,
+    y: height
   };
-  initHeader = function() {
-    var c, x;
-    width = 500;
-    height = 500;
-    target = {
-      x: 0,
-      y: height
+  canvas = document.getElementById("demo-canvas");
+  canvas.width = width;
+  canvas.height = height;
+  canvas.addEventListener("mouseover", onMouseOver);
+  ctx = canvas.getContext("2d");
+  circles = {};
+  addCircles();
+  return animate();
+};
+
+addListeners = function() {
+  return window.addEventListener("scroll", scrollCheck);
+};
+
+scrollCheck = function() {
+  if (document.body.scrollTop > height) {
+    return animateHeader = false;
+  } else {
+    return animateHeader = true;
+  }
+};
+
+resize = function() {
+  width = window.innerWidth;
+  height = window.innerHeight;
+  canvas.width = width;
+  return canvas.height = height;
+};
+
+addCircles = function() {
+  var c, count, _results;
+  count = 0;
+  _results = [];
+  while (count < width * 0.09) {
+    c = new Circle(x);
+    circles[x] = c;
+    x++;
+    _results.push(count++);
+  }
+  return _results;
+};
+
+onMouseOver = function() {
+  return addCircles();
+};
+
+firstRun = true;
+
+animate = function() {
+  var circle, i;
+  if (animateHeader) {
+    ctx.clearRect(0, 0, width, height);
+    for (i in circles) {
+      circle = circles[i];
+      circle.draw();
+    }
+  }
+  return requestAnimationFrame(animate);
+};
+
+Circle = (function() {
+  function Circle(id) {
+    this.id = id;
+    this.pos = {};
+    this.init();
+  }
+
+  Circle.prototype.init = function() {
+    this.pos.x = getRandom(0, width);
+    this.pos.y = getRandom(200, height);
+    this.alpha = 0.1 + Math.random() * 0.9;
+    this.scale = 0.1 + Math.random() * 0.05;
+    this.life = 20 + Math.random() * 10;
+    this.remaining_life = this.life;
+    return this.speed = {
+      x: -2.5 + Math.random() * 5,
+      y: -5 + Math.random()
     };
-    canvas = document.getElementById("demo-canvas");
-    canvas.width = width;
-    canvas.height = height;
-    ctx = canvas.getContext("2d");
-    circles = [];
-    x = 0;
-    while (x < width * 0.04) {
-      c = new Circle();
-      circles.push(c);
-      x++;
-    }
-    return animate();
   };
-  addListeners = function() {
-    return window.addEventListener("scroll", scrollCheck);
-  };
-  scrollCheck = function() {
-    if (document.body.scrollTop > height) {
-      return animateHeader = false;
-    } else {
-      return animateHeader = true;
-    }
-  };
-  resize = function() {
-    width = window.innerWidth;
-    height = window.innerHeight;
-    canvas.width = width;
-    return canvas.height = height;
-  };
-  firstRun = true;
-  animate = function() {
-    var i, _fn;
-    if (animateHeader) {
-      ctx.clearRect(0, 0, width, height);
-      if (firstRun) {
-        _fn = function(i) {
-          return setTimeout((function() {
-            return circles[i].draw();
-          }), 120 * i);
-        };
-        for (i in circles) {
-          _fn(i);
-        }
-        setTimeout((function() {
-          return firstRun = false;
-        }), circles.length * 120);
-      } else {
-        for (i in circles) {
-          circles[i].draw();
-        }
-      }
-    }
-    return requestAnimationFrame(animate);
-  };
-  Circle = (function() {
-    function Circle() {
-      this.pos = {};
-      this.init();
-      console.log(this);
-    }
 
-    Circle.prototype.init = function() {
-      this.pos.x = getRandom(100, width - 100);
-      this.pos.y = getRandom(200, height);
-      this.alpha = 0.1 + Math.random() * 0.9;
-      this.scale = 0.1 + Math.random() * 0.05;
-      this.life = 20 + Math.random() * 10;
-      this.remaining_life = this.life;
-      return this.speed = {
-        x: -2.5 + Math.random() * 5,
-        y: -5 + Math.random()
-      };
-    };
+  Circle.prototype.draw = function() {
+    this.scale -= 0.00065;
+    this.alpha -= 0.005;
+    this.remaining_life -= 0.1;
+    if (this.remaining_life < 0 || this.scale < 0) {
+      delete circles[this.id];
+      return;
+    }
+    this.pos.y += this.speed.y;
+    this.pos.x += this.speed.x;
+    ctx.beginPath();
+    ctx.arc(this.pos.x, this.pos.y, this.scale * 10, 0, 2 * Math.PI, false);
+    ctx.fillStyle = "rgba(254,149,0," + this.alpha + ")";
+    return ctx.fill();
+  };
 
-    Circle.prototype.draw = function() {
-      this.scale -= 0.00065;
-      this.alpha -= 0.005;
-      this.remaining_life -= 0.1;
-      if (this.remaining_life < 0 || this.scale < 0) {
-        this.init();
-      }
-      this.pos.y += this.speed.y;
-      this.pos.x += this.speed.x;
-      ctx.beginPath();
-      ctx.arc(this.pos.x, this.pos.y, this.scale * 10, 0, 2 * Math.PI, false);
-      ctx.fillStyle = "rgba(254,149,0," + this.alpha + ")";
-      return ctx.fill();
-    };
+  return Circle;
 
-    return Circle;
-
-  })();
-  initHeader();
-  return addListeners();
 })();
+
+initHeader();
+
+addListeners();
--- src/script.coffee
+++ src/script.coffee
@@ -1,114 +1,125 @@
 scene = document.getElementById 'scene'
 parallax = new Parallax scene
 
-(->
+# (->
        
-  width = undefined
-  height = undefined
-  largeHeader = undefined
-  canvas = undefined
-  ctx = undefined
-  circles = undefined
-  target = undefined
-  animateHeader = true
+width = undefined
+height = undefined
+largeHeader = undefined
+canvas = undefined
+ctx = undefined
+circles = undefined
+target = undefined
+animateHeader = true
+
+getRandom = (min, max) ->
+  Math.floor(Math.random() * (max - min + 1)) + min
+
+x = 0
+initHeader = ->
+  # width = window.innerWidth
+  # height = window.innerHeight
+  width = 300
+  height = 500
+  target =
+    x: 0
+    y: height
+
+  # largeHeader = document.getElementById("large-header")
+  # largeHeader.style.height = height + "px"
+  canvas = document.getElementById("demo-canvas")
+  canvas.width = width
+  canvas.height = height
+  canvas.addEventListener "mouseover", onMouseOver
+  ctx = canvas.getContext("2d")
   
-  getRandom = (min, max) ->
-    Math.floor(Math.random() * (max - min + 1)) + min
+  # create particles
+  circles = {}
 
-  initHeader = ->
-    # width = window.innerWidth
-    # height = window.innerHeight
-    width = 500
-    height = 500
-    target =
-      x: 0
-      y: height
+  addCircles()
+  animate()
 
-    # largeHeader = document.getElementById("large-header")
-    # largeHeader.style.height = height + "px"
-    canvas = document.getElementById("demo-canvas")
-    canvas.width = width
-    canvas.height = height
-    ctx = canvas.getContext("2d")
-    
-    # create particles
-    circles = []
-    x = 0
+# Event handling
+addListeners = ->
+  window.addEventListener "scroll", scrollCheck
+  # window.addEventListener "resize", resize
 
-    while x < width * 0.04
-      c = new Circle()
-      circles.push c
-      x++
-    animate()
-  
-  # Event handling
-  addListeners = ->
-    window.addEventListener "scroll", scrollCheck
-    # window.addEventListener "resize", resize
+scrollCheck = ->
+  if document.body.scrollTop > height
+    animateHeader = false
+  else
+    animateHeader = true
 
-  scrollCheck = ->
-    if document.body.scrollTop > height
-      animateHeader = false
-    else
-      animateHeader = true
+resize = ->
+  width = window.innerWidth
+  height = window.innerHeight
+  # largeHeader.style.height = height + "px"
+  canvas.width = width
+  canvas.height = height
 
-  resize = ->
-    width = window.innerWidth
-    height = window.innerHeight
-    # largeHeader.style.height = height + "px"
-    canvas.width = width
-    canvas.height = height
 
-  firstRun = true
-  animate = ->
-    if animateHeader
-      ctx.clearRect 0, 0, width, height
-      if firstRun
-        for i of circles
-          ((i)->
-            setTimeout (->
-              circles[i].draw()
-              ), 120 * i
-            )(i)
-        setTimeout (->
-            firstRun = false
-          ), circles.length * 120
-      else
-        for i of circles
-          circles[i].draw()
-    requestAnimationFrame animate
-  
-  class Circle
-    constructor: ->
-      @pos = {}
-      @init()
-      console.log @
+addCircles = ->
+  count = 0
+  while count < width * 0.09
+    c = new Circle(x)
+    circles[x] = c
+    x++
+    count++
 
-    init: ->
-      @pos.x = getRandom(100, width - 100)
-      @pos.y = getRandom(200, height)
-      # @pos.x = Math.random() * width
-      # @pos.y = height + Math.random() * 300
-      @alpha = 0.1 + Math.random() * 0.9
-      @scale = 0.1 + Math.random() * 0.05
-      @life = 20+Math.random()*10;
-      @remaining_life = @life;
-      @speed = 
-        x: -2.5+Math.random() * 5
-        y: -5 + Math.random()
+onMouseOver =  ->
+  addCircles()
 
-    draw: ->
-      @scale -= 0.00065
-      @alpha -= 0.005
-      @remaining_life -= 0.1
-      @init()  if @remaining_life < 0 or @scale < 0
-      @pos.y += @speed.y
-      @pos.x += @speed.x
-      ctx.beginPath()
-      ctx.arc @pos.x, @pos.y, @scale * 10, 0, 2 * Math.PI, false
-      ctx.fillStyle = "rgba(254,149,0," + @alpha + ")"
-      ctx.fill()
+firstRun = true
+animate = ->
+  if animateHeader
+    ctx.clearRect 0, 0, width, height
+    # if firstRun
+    #   for i of circles
+    #     ((i)->
+    #       setTimeout (->
+    #         circles[i].draw()
+    #         ), 120 * i
+    #       )(i)
+    #   setTimeout (->
+    #       firstRun = false
+    #     ), circles.length * 120
+    # else
+    for i, circle of circles
+      circle.draw()
+  requestAnimationFrame animate
 
-  initHeader()
-  addListeners()
-)()
+class Circle
+  constructor: (@id) ->
+    @pos = {}
+    @init()
+
+  init: ->
+    @pos.x = getRandom(0, width)
+    @pos.y = getRandom(200, height)
+    @alpha = 0.1 + Math.random() * 0.9
+    @scale = 0.1 + Math.random() * 0.05
+    @life = 20+Math.random()*10;
+    @remaining_life = @life;
+    @speed = 
+      x: -2.5+Math.random() * 5
+      y: -5 + Math.random()
+
+  draw: ->
+    @scale -= 0.00065
+    @alpha -= 0.005
+    @remaining_life -= 0.1
+    if @remaining_life < 0 or @scale < 0
+      delete circles[@id]
+      # circles.splice(@id,1)
+      # console.log circles.length
+      return
+    @pos.y += @speed.y
+    @pos.x += @speed.x
+    ctx.beginPath()
+    ctx.arc @pos.x, @pos.y, @scale * 10, 0, 2 * Math.PI, false
+    ctx.fillStyle = "rgba(254,149,0," + @alpha + ")"
+    ctx.fill()
+
+initHeader()
+addListeners()
+# )()
